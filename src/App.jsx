4de4c379b283a12dfb4116e9fe02756e8eb68787
Adiksuu <codeadiksuu@gmail.com>
2024-11-14 18:00:18 +0100

    Connected with database

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { BrowserRouter } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Router from "./views/Router";
+import Loading from "./views/Loading";
+import { useEffect, useState } from "react";
+import { auth } from "./api/database/connect";
+import { _loginAsGuest } from "./api/database/_loginAsGuest";
 
 function App() {
-    return (
+    const [authorized, setAuthorized] = useState(false);
+
+    useEffect(() => {
+        if (authorized) return;
+
+        _loginAsGuest(setAuthorized);
+    }, [authorized]);
+
+    return authorized ? (
         <>
-        <BrowserRouter>
-            <Sidebar />
-            <Router />
-        </BrowserRouter>
+            <BrowserRouter>
+                <Sidebar />
+                <Router />
+            </BrowserRouter>
         </>
+    ) : (
+        <Loading />
     );
 }
 
---
+++ src/views/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../assets/images/logo.png'
+
+export default function Loading() {
+  return (
+    <section className="loading">
+        <img src={logo} alt="logo of app" />
+    </section>
+  )
+}
