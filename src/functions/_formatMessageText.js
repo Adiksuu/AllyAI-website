36d61b64d50ac011dcfc3926376819dec99f9eec
Adiksuu <codeadiksuu@gmail.com>
2024-11-17 15:08:14 +0100

    Fixed text rendering

--- src/functions/_formatMessageText.js
+++ src/functions/_formatMessageText.js
@@ -1,6 +1,12 @@
-const _formatMessageText = (text) => {
-    const replacedText = text.replace(/\.  (?=\S)/g, ".\n");
-    return replacedText.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
+export const _formatMessageText = (text) => {
+    // Zamiana `**tekst**` na <strong>tekst</strong>
+    const strongFormatted = text.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
+    
+    // Escapowanie innych tagów HTML (zamiana < i > na &lt; i &gt;)
+    const escapedHTML = strongFormatted.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    
+    // Przywrócenie poprawnego renderowania tagów <strong>
+    const restoredStrong = escapedHTML.replace(/&lt;strong&gt;/g, "<strong>").replace(/&lt;\/strong&gt;/g, "</strong>");
+    
+    return restoredStrong;
 };
-
-export { _formatMessageText }
