0ed8b0907dc63758d3081c1b64f5a07b66bfb169
Adiksuu <codeadiksuu@gmail.com>
2024-11-18 17:28:04 +0100

    Auto scrolling to bottom

--- src/components/Chat/Messages.jsx
+++ src/components/Chat/Messages.jsx
@@ -1,37 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import Message from './Message'
-import { _loadMessages } from '../../functions/_loadMessages'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState, useRef } from 'react';
+import Message from './Message';
+import { _loadMessages } from '../../functions/_loadMessages';
+import { useNavigate } from 'react-router-dom';
 import logo from "../../assets/images/logo.png";
 
 export default function Messages({ setHistory, id, loading }) {
-    const [messages, setMessages] = useState([])
-    const navigate = useNavigate()
-    
+    const [messages, setMessages] = useState([]);
+    const navigate = useNavigate();
+    const messagesContainerRef = useRef(null);
+
     useEffect(() => {
         const loadChat = async () => {
-            const result = await _loadMessages(setMessages, id, setHistory)
+            const result = await _loadMessages(setMessages, id, setHistory);
             if (result === 'back') {
-                navigate('/')
+                navigate('/');
             }
+        };
+        loadChat();
+    }, [id, navigate, setHistory]);
+
+    useEffect(() => {
+        if (messagesContainerRef.current) {
+            messagesContainerRef.current.scrollTo({
+                top: messagesContainerRef.current.scrollHeight,
+                behavior: "smooth",
+            });
         }
-        loadChat()
-    }, [id, navigate])
+    }, [messages]);
 
     function LoadingEffect() {
         return (
-            <div className={'message wait'}>
+            <div className="message wait">
                 <div className="leftside">
                     <img src={logo} alt="logo of user" />
                 </div>
             </div>
-        )
+        );
     }
 
-  return (
-    <div className="messages">
-        {messages.map((message, index) => <Message message={message} messagePath={id} key={index} />)}
-        {loading ? <LoadingEffect /> : null}
-    </div>
-  )
+    return (
+        <div className="messages" ref={messagesContainerRef}>
+            {messages.map((message, index) => (
+                <Message message={message} messagePath={id} key={index} />
+            ))}
+            {loading ? <LoadingEffect /> : null}
+        </div>
+    );
 }
