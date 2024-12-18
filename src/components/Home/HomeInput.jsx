a92a3eb0c76ff739991727b83e2041bb5d8f86ed
Adiksuu <codeadiksuu@gmail.com>
2024-12-18 16:35:34 +0100

    Home input into textarea

--- src/components/Home/HomeInput.jsx
+++ src/components/Home/HomeInput.jsx
@@ -1,6 +1,6 @@
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { _sendMessage } from '../../functions/_sendMessage'
 import { useNavigate } from 'react-router-dom'
 import { models } from '../../api/models/modelsList'
@@ -10,6 +10,7 @@ export default function HomeInput({ model }) {
     const [message, setMessage] = useState('')
     const [loading, setLoading] = useState(false)
     const [prompts, setPrompts] = useState(0)
+    const textareaRef = useRef(null);
     const navigate = useNavigate()
 
     const handleNewChat = async (e) => {
@@ -23,11 +24,24 @@ export default function HomeInput({ model }) {
         navigate(`/chat/${currentChat}`, { state: { loading: true } });
     }
 
+    const adjustTextareaHeight = () => {
+        const textarea = textareaRef.current;
+        if (textarea) {
+            textarea.style.height = "auto";
+            textarea.style.height = `${textarea.scrollHeight - 20}px`;
+        }
+    };
+
     const handleKeyDown = (e) => {
         if (e.key === 'Enter' && !e.shiftKey) {
             e.preventDefault();
             handleNewChat(e);
         }
+    }
+
+    const handleInput = (e) => {
+        setMessage(e.target.value)
+        adjustTextareaHeight()
     }
 
     useEffect(() => {
@@ -39,7 +53,7 @@ export default function HomeInput({ model }) {
 
     return (
         <form className="input" onSubmit={(e) => handleNewChat(e)}>
-            <input disabled={prompts >= 50 || loading} type="text" onKeyDown={handleKeyDown} placeholder={prompts >= 50 ? 'Prompts limit reached for today': loading ? 'Wait for response...' : 'Ask question'} value={message} onChange={(e) => setMessage(e.target.value)} />
+            <textarea ref={textareaRef} disabled={prompts >= 50 || loading} type="text" onKeyDown={handleKeyDown} placeholder={prompts >= 50 ? 'Prompts limit reached for today': loading ? 'Wait for response...' : 'Ask question'} value={message} onChange={(e) => handleInput(e)} />
             <button type='submit'><FontAwesomeIcon icon={faPaperPlane} /></button>
         </form>
     )
