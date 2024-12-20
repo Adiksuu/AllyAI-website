9f5b4118b75d29f65b11f4d187fcb20feac5c4c6
Adiksuu <codeadiksuu@gmail.com>
2024-12-20 18:33:37 +0100

    changed logout message

--- src/components/Settings/UserSettings.jsx
+++ src/components/Settings/UserSettings.jsx
@@ -7,7 +7,7 @@ export default function UserSettings() {
         <h2>User settings</h2>
         <div className="list">
             <span>Logout</span>
-            <p>Log out of your account to prevent unwanted authorization.</p>
+            <p>Log out of your account to prevent unwanted authorization, you will not be automatically logged in the next time you visit the site</p>
             <button onClick={() => _userLogout()}>LOGOUT</button>
         </div>
     </div>
