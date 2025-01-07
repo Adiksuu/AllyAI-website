8ebf3157fc1e103d64e5fb424865c1dc5800343c
Adiksuu <codeadiksuu@gmail.com>
2025-01-07 20:30:45 +0100

    Design changes of popups

--- src/components/Home/NewFeatures.jsx
+++ src/components/Home/NewFeatures.jsx
@@ -1,23 +1,36 @@
-import { faCodeBranch, faCrown } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { faCodeBranch, faCrown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function NewFeatures({ isPremium, experimental }) {
-  return !isPremium ? (
-    <div className="new_version">
-        <div className="left"><FontAwesomeIcon icon={faCrown} /></div>
-        <div className="right">
-            <h3>Premium account available</h3>
-            <p>Get access to additional features, unlimited prompts and more</p>
-        </div>
-    </div>
-  ) : !experimental ? (
-    <div className="new_version">
-        <div className="left"><FontAwesomeIcon icon={faCodeBranch} /></div>
-        <div className="right">
-            <h3>New features available</h3>
-            <p>Check out the latest features in the settings tab</p>
-        </div>
-    </div>
-  ) : null
+    const navigate = useNavigate()
+
+    function Popup({ icon, title, description }) {
+        return (
+            <div className="new_version" onClick={() => navigate('/settings')} >
+                <div className="left">
+                    <FontAwesomeIcon icon={icon} />
+                </div>
+                <div className="right">
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </div>
+            </div>
+        );
+    }
+
+    return !isPremium ? (
+        <Popup
+            icon={faCrown}
+            title="Premium account available"
+            description="Get access to additional features, unlimited prompts and more"
+        />
+    ) : !experimental ? (
+        <Popup
+            icon={faCodeBranch}
+            title="Experimental features"
+            description="Try out the latest features in the settings tab"
+        />
+    ) : null;
 }
